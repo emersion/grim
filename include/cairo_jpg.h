7d2f5aca85d2b6abfb420f64a5b1a8981f17e251
meak <meak@fastmail.com>
2018-07-28 09:50:18 +0200

    fix: header function rename
    cairo_image_surface...jpeg_mem to cairo_surface...jpeg_mem

--- include/cairo_jpg.h
+++ include/cairo_jpg.h
@@ -3,7 +3,7 @@
 
 #include <cairo.h>
 
-cairo_status_t cairo_image_surface_write_to_jpeg_mem(cairo_surface_t *sfc, unsigned char **data, size_t *len, int quality);
+cairo_status_t cairo_surface_write_to_jpeg_mem(cairo_surface_t *sfc, unsigned char **data, size_t *len, int quality);
 cairo_status_t cairo_surface_write_to_jpeg_stream(cairo_surface_t *sfc, cairo_write_func_t write_func, void *closure, int quality);
 cairo_status_t cairo_surface_write_to_jpeg(cairo_surface_t *sfc, const char *filename, int quality);
 
