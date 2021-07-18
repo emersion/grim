5b9c12032df905961f543f8015e7280fa31e9d01
Lars Lenckowski <larslenckowski@gmail.com>
2021-07-18 12:16:54 +0200

    add bash completion
    only check optional deps when the features are enabled

---
+++ contrib/completions/bash/grim.bash
@@ -0,0 +1,27 @@
+_grim() {
+	_init_completion || return
+
+	CUR="${COMP_WORDS[COMP_CWORD]}"
+	PREV="${COMP_WORDS[COMP_CWORD-1]}"
+
+	if [[ "$PREV" == "-t" ]]; then
+		COMPREPLY=($(compgen -W "png ppm jpeg" -- "$CUR"))
+		return
+	elif [[ "$PREV" == "-o" ]]; then
+		OUTPUTS="$(swaymsg -t get_outputs 2>/dev/null | \
+			jq -r '.[] | select(.active) | "\(.name)\t\(.make) \(.model)"' 2>/dev/null)"
+
+		COMPREPLY=($(compgen -W "$OUTPUTS" -- "$CUR"))
+		return
+	fi
+
+	if [[ "$CUR" == -* ]]; then
+		COMPREPLY=($(compgen -W "-h -s -g -t -q -o -c" -- "$CUR"))
+		return
+	fi
+
+	# fall back to completing filenames
+	_filedir
+}
+
+complete -F _grim grim
