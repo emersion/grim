1fa1ece6cf5d0025b9fe1de0f4d5fae3d5f72b26
Manuel Stoeckl <code@mstoeckl.com>
2021-10-08 18:55:48 -0400

    write_jpg: switch from cairo_surface_t to pixman_image_t

--- include/write_jpg.h
+++ include/write_jpg.h
@@ -1,9 +1,9 @@
 #ifndef _WRITE_JPEG_H
 #define _WRITE_JPEG_H
 
-#include <cairo.h>
+#include <pixman.h>
 #include <stdio.h>
 
-int cairo_surface_write_to_jpeg_stream(cairo_surface_t *sfc, FILE *stream, int quality);
+int write_to_jpeg_stream(pixman_image_t *image, FILE *stream, int quality);
 
 #endif
--- write_jpg.c
+++ write_jpg.c
@@ -11,26 +11,26 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <unistd.h>
-#include <cairo.h>
 #include <jpeglib.h>
 
 #include "write_jpg.h"
 
-int cairo_surface_write_to_jpeg_stream(cairo_surface_t *sfc,
-		FILE *stream, int quality) {
+int write_to_jpeg_stream(pixman_image_t *image, FILE *stream, int quality) {
+	pixman_format_code_t format = pixman_image_get_format(image);
+	assert(format == PIXMAN_a8r8g8b8 || format == PIXMAN_x8r8g8b8);
+
 	struct jpeg_compress_struct cinfo;
 	struct jpeg_error_mgr jerr;
 	JSAMPROW row_pointer[1];
-
 	cinfo.err = jpeg_std_error(&jerr);
 	jpeg_create_compress(&cinfo);
 
 	unsigned char *data = NULL;
 	unsigned long len = 0;
 	jpeg_mem_dest(&cinfo, &data, &len);
-	cinfo.image_width = cairo_image_surface_get_width(sfc);
-	cinfo.image_height = cairo_image_surface_get_height(sfc);
-	if (cairo_image_surface_get_format(sfc) == CAIRO_FORMAT_ARGB32) {
+	cinfo.image_width = pixman_image_get_width(image);
+	cinfo.image_height = pixman_image_get_height(image);
+	if (format == PIXMAN_a8r8g8b8) {
 		cinfo.in_color_space = JCS_EXT_BGRA;
 	} else {
 		cinfo.in_color_space = JCS_EXT_BGRX;
@@ -43,8 +43,8 @@ int cairo_surface_write_to_jpeg_stream(cairo_surface_t *sfc,
 	jpeg_start_compress(&cinfo, TRUE);
 
 	while (cinfo.next_scanline < cinfo.image_height) {
-		row_pointer[0] = cairo_image_surface_get_data(sfc) + (cinfo.next_scanline
-				* cairo_image_surface_get_stride(sfc));
+		row_pointer[0] = (unsigned char *)pixman_image_get_data(image)
+			+ (cinfo.next_scanline * pixman_image_get_stride(image));
 		(void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
 	}
 
