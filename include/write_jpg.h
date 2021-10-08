6947e6d43d3c9fa4dfcebd3ccd817267fdcce0c3
Manuel Stoeckl <code@mstoeckl.com>
2021-10-08 18:35:25 -0400

    write_jpg: use FILE* in cairo_surface_write_to_jpeg_stream

--- include/write_jpg.h
+++ include/write_jpg.h
@@ -2,7 +2,8 @@
 #define _WRITE_JPEG_H
 
 #include <cairo.h>
+#include <stdio.h>
 
-cairo_status_t cairo_surface_write_to_jpeg_stream(cairo_surface_t *sfc, cairo_write_func_t write_func, void *closure, int quality);
+cairo_status_t cairo_surface_write_to_jpeg_stream(cairo_surface_t *sfc, FILE *stream, int quality);
 
 #endif
